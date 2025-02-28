cac6dec13478b8e6371973703b58b1da1c398c46
emmanuelokoye <tricelex@gmail.com>
2025-02-28 23:59:05 +0100

    added register controllers 5.3.1

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -2,9 +2,14 @@ package tacos.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import tacos.User;
+import tacos.data.UserRepository;
 
 @Configuration
 public class SecurityConfig {
@@ -14,7 +19,25 @@ public class SecurityConfig {
     return new BCryptPasswordEncoder();
   }
 
-  public interface UserDetailsService() {
-    return new JdbcUserDetailsService();
+  @Bean
+  public UserDetailsService userDetailsService(UserRepository userRepository) {
+    return username -> {
+      User user = userRepository.findByUsername(username);
+      if (user != null) {
+        return user;
+      }
+      throw new UsernameNotFoundException("User '" + username + "' not found");
+    };
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http.authorizeHttpRequests()
+        .antMatchers("/design", "/orders")
+        .hasRole("USER")
+        .antMatchers("/", "/**")
+        .permitAll()
+        .and()
+        .build();
   }
 }
